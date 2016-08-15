044817dff86b799a26f226e4dcffe1a0cdcb9a5a
Bogdan Coticopol <bcoticopol@gmail.com>
2016-08-15 11:11:31 +0300

    general functions (appCommons)

--- App/app.js
+++ App/app.js
@@ -3,15 +3,16 @@ const AuctionHouseWebLoader = require('./AuctionHouse/AuctionHouseWebLoader');
 const Auctions = require('./AuctionHouse/auctions.js');
 const log = require('single-line-log').stdout;
 const sleep = require('sleep');
+const AppCommons = require('./Helpers/appCommons');
+
+var appCommons = new AppCommons();
 
 function _main() {
-    console.log("app.js: _main() function called, starting database update.");
+    console.log("%s app.js: _main() function called, starting database update.", appCommons.getDateTime());
     var mongodb = new MongoDBHelper();
     var ahwl = new AuctionHouseWebLoader();
 
     mongodb.connect(function () {
-
-        //insert
         ahwl.getAuctionHouseFile(function () {
             ahwl.readAuctionHouseFiles(function () {
                 var auctions = new Auctions();
@@ -37,5 +38,6 @@ function recursiveCall() {
 
 console.log("WoW Auction House Data Loader v.0.1");
 _main();
+
 
 
