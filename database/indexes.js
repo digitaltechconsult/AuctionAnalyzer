7dbe5ddc1c64a92de911e8cf0ff5b5c9e71a167a
Bogdan Coticopol <bcoticopol@gmail.com>
2016-08-25 21:14:16 +0300

    Database Index Script

---
+++ database/indexes.js
@@ -0,0 +1,9 @@
+// This should be executed in a mongo db env
+
+//auctions_Dentarg
+db.auctions_Dentarg.dropIndexes();
+
+db.auctions_Dentarg.createIndex({timestamp:-1});
+db.auctions_Dentarg.createIndex({item:1});
+db.auctions_Dentarg.createIndex({owner:1, ownerRealm:1});
+
