01d3931716991b08b745382d0c77dc38c0ec5b07
Bogdan Coticopol <bcoticopol@gmail.com>
2016-08-25 21:23:16 +0300

    Removed char_loader.js from automatic launch, takes too much time

--- bin/data_loader.js
+++ bin/data_loader.js
@@ -1,4 +1,4 @@
-require( "console-stripe" )( console );
+require("console-stripe")(console);
 const AuctionLoader = require('../app/AuctionsLoader');
 const AppCommons = require('../lib/AppCommons');
 
@@ -7,16 +7,12 @@ var appCommons = new AppCommons();
 function _main() {
     console.info("Data Loader Process started");
     var auctionLoader = new AuctionLoader();
-    auctionLoader.loadAuctions(function(){
+    auctionLoader.loadAuctions(function () {
 
         console.info("Starting Item Loader script");
-        appCommons.runScript('./item_loader.js', function(err) {
+        appCommons.runScript('./item_loader.js', function (err) {
             console.info("Item Loader script finished");
-            console.info("Starting Char Loader script");
-            appCommons.runScript('./char_loader.js', function(err) {
-               console.info("Char Loader script finished");
-                appCommons.makeRecursiveCall(_main);
-           });
+            appCommons.makeRecursiveCall(_main);
         });
     });
 }
