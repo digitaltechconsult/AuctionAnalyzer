29b1ceca4c8c7bf9dce4f145268100cdee7f0463
Bogdan Coticopol <bcoticopol@gmail.com>
2016-08-26 00:25:02 +0300

    Some analytics...but I guess MySQL is the key

--- analytics/DailySnapshot.js
+++ analytics/DailySnapshot.js
@@ -2,21 +2,63 @@ require('console-stripe')(console);
 let MongoClient = require('../lib/MongoDbHelper');
 let AppSettings = require('../app/AppSettings');
 
-function DailySnapshot() {}
+function DailySnapshot() { }
 
-DailySnapshot.prototype.generateSnapshot = function(lastXDays = 0) {
+DailySnapshot.prototype.generateSnapshot = function (startDate = 7, endDate = 0, onSuccess, onError) {
     var mongoClient = new MongoClient();
     mongoClient.connect(onConnect, onError);
 
-    function onConnect() {
-        console.info("Connected to database");
-        var collectionName = 'auctions_' + AppSettings.realm;
-    }
+    var startTimestamp = Date.now() - (3600 * 1000 * 24 * startDate);
+    var endTimestamp = Date.now() - (3600 * 1000 * 24 * endDate);
 
-    function onError() {
-        console.error("Cannot connect to database");
+    function onConnect() {
+        mongoClient.db.runCommand(
+            {
+                'aggregate': 'auctions_' + AppSettings.realm,
+                'pipeline': [
+                    {
+                        '$match': { 'timestamp': { '$gte': startTimestamp, '$lte': endTimestamp } }
+                    },
+                    {
+                        '$group': {
+                            '_id': { 'item': '$item', 'timestamp': '$timestamp' },
+
+                            'bidders': { '$sum': 1 },
+                            'qty': { '$sum': '$quantity' },
+
+                            'total_bid': { '$sum': '$bid' },
+                            'total_buyout': { '$sum': '$buyout' },
+
+                            'max_bid': { '$max': '$bid' },
+                            'max_buyout': { '$max': '$buyout' },
+
+                            'min_bid': { '$min': '$bid' },
+                            'min_buyout': { '$min': '$buyout' },
+
+                            'average_bid': { '$avg': '$bid' },
+                            'average_buyout': { '$avg': '$buyout' }
+                        }
+                    },
+                    {
+                        '$sort': { 'timestamp': -1, 'qty': -1 }
+                    }],
+                    'allowDiskUse':true
+            }
+
+            , function (error, results) {
+                if (error !== null) {
+                    console.error("Error while executing aggregate function.");
+                    onError();
+                } else {
+                    console.info("Operation was successfull.");
+                    onSuccess(results);
+                }
+            });
     }
 
 }
 
 module.exports = DailySnapshot;
+
+var x = new DailySnapshot();
+x.generateSnapshot();
