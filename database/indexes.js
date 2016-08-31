47904e9c8420a4ba8477b77f5018ff8a6311bb09
Bogdan Coticopol <bcoticopol@gmail.com>
2016-08-31 14:35:45 +0300

    CSV Export

--- database/indexes.js
+++ database/indexes.js
@@ -8,3 +8,10 @@ db.auctions_Dentarg.createIndex({item:1});
 db.auctions_Dentarg.createIndex({owner:1, ownerRealm:1});
 db.auctions_Dentarg.createIndex({timestamp:-1, item:1});
 
+//For CSV export
+db.getCollection('auctions_Dentarg').find({timestamp: {$type:1}}).forEach(function(row){
+    row.timestamp = NumberLong(row.timestamp);
+    db.getCollection('auctions_Dentarg').save(row);
+    });
+    
+//mongoexport --db AuctionHouse --collection auctions_Dentarg --type=csv --fieldFile fields.txt --out ./auctions_Dentarg.csv
